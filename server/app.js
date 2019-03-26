f9d82193f319391be9012e5f63835484584dca7e
Jie <1007172767@qq.com>
2019-03-26 22:14:23 +0800

    接收请求

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const Koa = require('koa');
-
+const koaBody = require('koa-body')
 const app = new Koa();
 
+app.use(koaBody());
 app.use(async(ctx)=>{
   // ctx.body = 'hello';
   let url = ctx.url;
@@ -9,13 +10,14 @@ app.use(async(ctx)=>{
   let req_query = request.query;
   let req_queryString = request.querystring;
 
+  console.log(ctx.request.body)
   ctx.body = {
       url,
       req_query,
       req_queryString
   }
 
-  console.log(ctx)
+  
 
 });
 
